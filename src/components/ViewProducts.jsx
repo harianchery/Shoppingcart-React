4424b28a4a53cfc4e95b3a5f0401af8f156ac97c
harianchery <hari.anchery001@gmail.com>
2024-06-07 21:37:09 +0530

    navbar created and added

--- src/components/ViewProducts.jsx
+++ src/components/ViewProducts.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const ViewProducts = () => {
     return (
         <div>
+            <Navbar/>
             <div className="containe">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
