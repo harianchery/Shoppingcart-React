055e105af7f889e628bd7e76d8c57b757dc13e50
harianchery <hari.anchery001@gmail.com>
2024-06-07 21:21:35 +0530

    search and delete file created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
+import SearchProduct from './components/SearchProduct';
+import DeleteProduct from './components/DeleteProduct';
 
 function App() {
   return (
     <div>
       <AddProduct/>
+      <SearchProduct/>
+      <DeleteProduct/>
     </div>
   );
 }
