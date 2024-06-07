4e6edaa20c42fb1316df08a4c4311d4215c6344f
harianchery <hari.anchery001@gmail.com>
2024-06-07 21:47:48 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import AddProduct from './components/AddProduct';
 import SearchProduct from './components/SearchProduct';
 import DeleteProduct from './components/DeleteProduct';
 import ViewProducts from './components/ViewProducts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddProduct/>
-      <SearchProduct/>
-      <DeleteProduct/>
-      <ViewProducts/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddProduct/>}/>
+      <Route path='/search' element={<SearchProduct/>}/>
+      <Route path='/delete' element={<DeleteProduct/>}/>
+      <Route path='/view' element={<ViewProducts/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
